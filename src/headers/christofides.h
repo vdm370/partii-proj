2e17bf52012d96e0e40049d5f1cd530e1cd88abc
VladaMG98 <vm370@cam.ac.uk>
2019-11-23 01:05:48 +0000

    Added some more interfaces - to be implemented

---
+++ src/headers/christofides.h
@@ -0,0 +1,5 @@
+#include "solution.h"
+#include "graph_dist.h"
+
+//n * n! bruteforce solution
+solution christofides(graph_dist g);
---
+++ src/headers/dyn_prog.h
@@ -0,0 +1,5 @@
+#include "solution.h"
+#include "graph_dist.h"
+
+//n * n! bruteforce solution
+solution dyn_prog(graph_dist g);
