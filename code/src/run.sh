c4a85a3cda0bda08d88ed59ca39d8de77849e796
vdm370 <vm370@cam.ac.uk>
2020-04-21 18:39:12 +0200

    added a lot of stuff

--- code/src/run.sh
+++ code/src/run.sh
@@ -6,20 +6,19 @@ if [[ $# -ne 3 ]]; then
 fi
 
 for prog in $1; do
-    OUTPUTS=""
-    SPACE=""
-    N_IN=0
-    for K in `seq 1 "$3"`; do
-        for input in $2; do
+    for input in $2; do
+        OUTPUTS=""
+        SPACE=""
+        N_IN=0
+        for K in `seq 1 "$3"`; do
             OUT=$("$prog" < "$input" | tr ' ' '\n' | tail -n 1)
             OUTPUTS="$OUTPUTS$SPACE$OUT"
             SPACE=" "
             N_IN=$((N_IN + 1))
         done
-    done
-
-    SUM=$(echo "$OUTPUTS" | tr ' ' + | bc)
-    AVG=$(echo "$SUM / $N_IN" | bc -l)
-    MIN=$(echo "$OUTPUTS" | tr ' ' '\n' | sort -g | head -n 1)
-    echo "$prog $AVG $MIN"
+        SUM=$(echo "$OUTPUTS" | tr ' ' + | bc)
+        AVG=$(echo "$SUM / $N_IN" | bc -l)
+        MIN=$(echo "$OUTPUTS" | tr ' ' '\n' | sort -g | head -n 1)
+        echo "$(basename $prog) $(basename $input) $AVG $MIN"
+    done   
 done
